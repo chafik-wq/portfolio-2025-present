8bd0885b657dee02683c0edc2c2fa4f95beb9c30
chafik <chafik22266@gmail.com>
2025-03-22 22:24:46 +0100

    react-tilt updated dotenv installed and configured

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense , useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
